95297515bb1671d25ce10d98565d61a5be71c2e7
Sergio <sergiotmg@gmail.com>
2018-10-25 13:53:09 +0200

    2.0

--- app/src/main/java/com/example/sergio/droidcafe/OrderActivity.java
+++ app/src/main/java/com/example/sergio/droidcafe/OrderActivity.java
@@ -3,7 +3,11 @@ package com.example.sergio.droidcafe;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class OrderActivity extends AppCompatActivity {
 
@@ -16,5 +20,40 @@ public class OrderActivity extends AppCompatActivity {
                 intent.getStringExtra(MainActivity.EXTRA_MESSAGE);
         TextView textView = findViewById(R.id.order_textview);
         textView.setText(message);
+        //para los radiobutton definimos el RadioGroup
+        RadioGroup radioGroup= findViewById(R.id.radioGroup);
+        radioGroup.check(R.id.sameday);
+    }
+
+    public void onRadioButtonClicked(View view) {
+        // Is the button now checked?
+        boolean checked = ((RadioButton) view).isChecked();
+        // Check which radio button was clicked.
+        switch (view.getId()) {
+            case R.id.sameday:
+                if (checked)
+                    // Same day service
+                    displayToast(getString(R.string.same_day_text));
+                break;
+            case R.id.nextday:
+                if (checked)
+                    // Next day delivery
+                    displayToast(getString(R.string.next_day_text));
+                break;
+            case R.id.pickup:
+                if (checked)
+                    // Pick up
+                    displayToast(getString(R.string.pick_up));
+                break;
+            default:
+                // Do nothing.
+                break;
+        }
+    }
+
+    private void displayToast(String message) {
+
+        Toast.makeText(getApplicationContext(), message,
+                Toast.LENGTH_SHORT).show();
     }
 }
