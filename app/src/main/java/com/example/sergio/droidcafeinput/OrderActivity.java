3903e2dc8400b71195572f3f7343cd962628c6d4
Sergio M <38441398+Sergiotmg@users.noreply.github.com>
2018-11-01 20:18:24 +0100

    4.0 se añade imeOptions="actionSend" en activity_order y se pone el codigo para que llame al pulsar a send en OrderAtivity.java

--- app/src/main/java/com/example/sergio/droidcafeinput/OrderActivity.java
+++ app/src/main/java/com/example/sergio/droidcafeinput/OrderActivity.java
@@ -1,11 +1,16 @@
 package com.example.sergio.droidcafeinput;
 
 import android.content.Intent;
+import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.KeyEvent;
 import android.view.View;
+import android.view.inputmethod.EditorInfo;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
+import android.widget.EditText;
 import android.widget.RadioButton;
 import android.widget.RadioGroup;
 import android.widget.Spinner;
@@ -16,6 +21,8 @@ import com.example.sergio.droidcafe.R;
 
 public class OrderActivity extends AppCompatActivity implements
         AdapterView.OnItemSelectedListener {
+    //crep la variable tag accesible desde cualquier instancia
+    private static final String TAG =MainActivity.class.getSimpleName();
     Spinner spinner;
 
     @Override
@@ -46,8 +53,51 @@ public class OrderActivity extends AppCompatActivity implements
         if (spinner != null) {
             spinner.setAdapter(adapter);
         }
-    }
+        //acciion para detectar telefono marcado yamrcar el numero inroducido
+        EditText editText = findViewById(R.id.phone_text);
+        if (editText != null)
+            editText.setOnEditorActionListener
+                    (new TextView.OnEditorActionListener() {
+                        @Override
+                        public boolean onEditorAction(TextView textView, int actionId, KeyEvent event) {
+                            boolean handled = false;
+                            if (actionId == EditorInfo.IME_ACTION_SEND) {
+                                dialNumber();
+                                handled = true;
+                            }
+                            return handled;
+                        }
+                        // If view is found, set the listener for editText.
+                    });
 
+    }//fin OnCreate
+    
+    //############ METODO LLAMAR POR TELEFONO ##############
+    private void dialNumber() {
+        // Find the editText_main view.
+        EditText editText = findViewById(R.id.phone_text);
+        String phoneNum = null;
+        // If the editText field is not null, 
+        // concatenate "tel: " with the phone number string.
+        if (editText != null) phoneNum = "tel:" +
+                editText.getText().toString();
+        // Optional: Log the concatenated phone number for dialing.
+        Log.d(TAG, "dialNumber: " + phoneNum);
+        // Specify the intent.
+        Intent intent = new Intent(Intent.ACTION_DIAL);
+        // Set the data for the intent as the phone number.
+        intent.setData(Uri.parse(phoneNum));
+        // If the intent resolves to a package (app),
+        // start the activity with the intent.
+        if (intent.resolveActivity(getPackageManager()) != null) {
+            startActivity(intent);
+        } else {
+            Log.d("ImplicitIntents", "No se puede llamar a este nº!");
+        }
+    }
+    //##########################################################
+    
+    
     public void onRadioButtonClicked(View view) {
         // Is the button now checked?
         boolean checked = ((RadioButton) view).isChecked();
